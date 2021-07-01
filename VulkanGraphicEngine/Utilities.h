7731777508477e339c79151fbfaaa0e58e77b6bd
Dragon44PL <krystian.pieniak44@gmail.com>
2021-07-01 17:01:08 +0200

    Created Vertex struct

--- VulkanGraphicEngine/Utilities.h
+++ VulkanGraphicEngine/Utilities.h
@@ -2,10 +2,18 @@
 
 #include <fstream>
 
+#include <glm/glm.hpp>
+
 const int MAX_FRAME_DRAWS = 2;
 
 const std::vector<const char* > deviceExtensions = {
     VK_KHR_SWAPCHAIN_EXTENSION_NAME
+};
+
+// Vertex data representation
+struct Vertex {
+    glm::vec3 pos;  // Vertex position (x, y, z)
+    glm::vec3 col;  // Vertex colour (r, g, b)
 };
 
 struct QueueFamilyIndices {
